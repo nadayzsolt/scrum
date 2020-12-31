b6047828c8e6f745276f5f09f603241b071ba726
nadayzsolt <nadayzsolt@gmail.com>
2020-12-31 04:29:08 +0100

    Add new project

---
+++ src/main/java/com/scrumpoker/scrumpoker/ScrumpokerApplication.java
@@ -0,0 +1,13 @@
+package com.scrumpoker.scrumpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumpokerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScrumpokerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/scrumpoker/scrumpoker/ScrumpokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrumpoker.scrumpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumpokerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
